88d1bf8261343c0cf59b63acec466a221c6ec3fc
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-08 11:10:28 -0500

    First tests. Just too  see if publish works.

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {Number} x The first number to add
+ * @param {Number} y The second number to add
+ * @returns The addition of x and y
+ */
+
+function add(x, y) {
+  return x + y;
+}
+
+add(1, 2);
