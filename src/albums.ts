002ed26599fb2154cad6e34ee321268f37021e16
Pravesh Kunwar <praveshisawesome2017@gmail.com>
2021-04-19 09:45:35 -0400

    (chore): Need to work on ISO code assigning in market.ts

--- src/albums.ts
+++ src/albums.ts
@@ -1,23 +1,30 @@
 import axios, { AxiosResponse } from 'axios';
 import { BASE } from './@types/constants';
+import { markets } from './@types/market';
 
 /**
  *
  * @param {String} token The auth token to request data.
  * @param {String | String[]} ids Id or ids of albums.
+ * @param {String} market The market for the album. (OPTIONAL)
  * @returns Promise<void | AxiosResponse<any>>
  */
 
 export const getMultipleAlbums = (
 	token: string,
-	ids: string | string[]
+	ids: string | string[],
+	market?: markets
 ): Promise<void | AxiosResponse<any>> => {
 	if (Array.isArray(ids)) {
-		return axios.get(BASE.url + `/albums?ids=${ids.join('%2C')}`, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+		return axios.get(
+			BASE.url +
+				`/albums?ids=${ids.join('%2C')}?market=${market ? market : 'US'}`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
 	} else if (!Array.isArray(ids)) {
 		return axios.get(BASE.url + `/albums?ids=${ids}`, {
 			headers: {
@@ -46,20 +53,27 @@ export const getMultipleAlbums = (
  *
  * @param {String} token The auth token to request data.
  * @param {String} id  The id of the specific album you want to get data for.
+ *  @param {String} market The market for the album. (OPTIONAL)
  * @returns Promsie<void | AxiosResponse<any>>
  */
 
 export const getAlbum = (
 	token: string,
-	id: string
+	id: string,
+	market?: markets
 ): Promise<void | AxiosResponse<any>> => {
 	if (typeof id !== 'string') {
 		throw new Error('Id must be string.');
 	} else if (typeof id === 'string') {
-		return axios.get(BASE.url + `/albums/${id}`, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
+		return axios.get(
+			BASE.url + `/albums/${id}?market=${market ? market : 'US'}`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
 	}
 };
+
+getMultipleAlbums('asd', 'asd');
